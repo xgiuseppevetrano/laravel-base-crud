b5a9d9d5863fada21655cf669da215361b153b0b
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-07-16 00:11:00 +0200

    refactoring controller

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -39,15 +39,7 @@ class ComicController extends Controller
     {
         $data = $request->all();
         $newComic = new Comic();
-
-        $newComic->title = $data["title"];
-        $newComic->description = $data["description"];
-        $newComic->image = $data["image"];
-        $newComic->price = $data["price"];
-        $newComic->series = $data["series"];
-        $newComic->sale_date = $data["sale_date"];
-        $newComic->type = $data["type"];
-
+        $newComic->fill($data);
         $newComic->save();
 
         return redirect()->route('comics.show', $newComic["id"]);
