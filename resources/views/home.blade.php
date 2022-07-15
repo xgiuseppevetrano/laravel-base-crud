158342876d569cf60a4f62a377e669634c507512
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-07-15 22:19:08 +0200

    add home page and home route

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.base')
+
+@section('title')
+    Dc Comics
+@endsection
+
+@section('page-content')
+    <main>
+        <div class="home">
+            <div class="container">
+                <h1 class="home__title">Welcome to DC Comics</h1>
+                <a href="{{route('comics.index')}}"><button class="home__btn">Discover our comics</button></a>
+            </div>
+        </div>
+    </main>
+@endsection
