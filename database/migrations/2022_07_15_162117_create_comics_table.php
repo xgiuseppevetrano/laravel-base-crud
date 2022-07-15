49e5ea2a9b309dc56a25495c01593cd9e53a171d
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-07-15 18:49:27 +0200

    add table & data table

---
+++ database/migrations/2022_07_15_162117_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',75);
+            $table->text('description');
+            $table->string('thumb')->nullable( );
+            $table->float('price', 4,2);
+            $table->string('serie',50);
+            $table->date('sale_date');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
